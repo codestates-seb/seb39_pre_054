256a099a63c690fb3adac63462e4f4f0736bdde3
crdrst777 <crdrst777@gmail.com>
2022-08-25 01:45:37 +0900

    컴포넌트 생성

---
+++ client/src/components/Footer.js
@@ -0,0 +1,17 @@
+// import axios from 'axios';
+import React from 'react';
+import styled from 'styled-components';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>Footer</div>;
+    </footer>
+  );
+};
+
+styled.button`
+  // <button> HTML 엘리먼트에 대한 스타일 정의
+`;
+
+export default Footer;

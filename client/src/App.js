877e913316b440e19ac08f7dc2341facb9363941
crdrst777 <crdrst777@gmail.com>
2022-08-25 15:33:06 +0900

    footer 1차 작업

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Main from "./pages/Main";
-import Ask from "./pages/Ask";
-import Questions from "./pages/Questions";
-import MyPage from "./pages/MyPage";
+import Main from './pages/Main';
+import Ask from './pages/Ask';
+import Questions from './pages/Questions';
+import MyPage from './pages/MyPage';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Main></Main>}></Route>
-          <Route path="/questions" element={<Questions></Questions>}></Route>
+          <Route path='/' element={<Main></Main>}></Route>
+          <Route path='/questions' element={<Questions></Questions>}></Route>
           {/* 질문보기 라우터 */}
           {/* <Route path="/questions/:id" element={<Main></Main>}></Route> */}
-          <Route path="/questions/ask" element={<Ask></Ask>}></Route>
-          <Route path="/users/:id" element={<MyPage></MyPage>}></Route>
+          <Route path='/questions/ask' element={<Ask></Ask>}></Route>
+          <Route path='/users/:id' element={<MyPage></MyPage>}></Route>
         </Routes>
       </Router>
+      <Footer></Footer>
     </>
   );
 };

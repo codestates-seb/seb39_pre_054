0e8e28336afdd59d1f25f31faec245d79255b4c3
YeomHyeJi <haji3354@naver.com>
2022-08-25 16:04:05 +0900

    login page 구현

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Main from "./pages/Main";
 import Ask from "./pages/Ask";
 import Questions from "./pages/Questions";
 import MyPage from "./pages/MyPage";
+import Login from "./pages/login.js"
+import Signup from "./pages/signup.js"
 
 const App = () => {
   return (
@@ -15,10 +17,13 @@ const App = () => {
           <Route path="/questions" element={<Questions></Questions>}></Route>
           {/* 질문보기 라우터 */}
           {/* <Route path="/questions/:id" element={<Main></Main>}></Route> */}
+          <Route path="/users/login" element = {<Login></Login>}></Route>
+          <Route path="/users/signup" element = {<Signup></Signup>}></Route>
           <Route path="/questions/ask" element={<Ask></Ask>}></Route>
           <Route path="/users/:id" element={<MyPage></MyPage>}></Route>
         </Routes>
       </Router>
+  
     </>
   );
 };

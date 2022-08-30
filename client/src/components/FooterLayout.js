b474e1bcc237d27aae6581e6649014623a0c5c06
homile <ssanho2@naver.com>
2022-08-30 13:26:15 +0900

    feat:outlet 기능 도입

---
+++ client/src/components/FooterLayout.js
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;

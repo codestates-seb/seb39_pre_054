59f02df35dc8323f0ff189dde634cfa12deae135
YeomHyeJi <haji3354@naver.com>
2022-08-30 16:45:38 +0900

    회원가입 수정

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import LeftSide from "../components/LeftSide";
+
 // 마이페이지
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (  
+<MainContainer>
+  <Left>
+  <LeftSide />
+  </Left>
+  <div>hi</div>
+
+</MainContainer>
+  );
 };
 
 export default MyPage;
+
+
+const Left = styled.div`
+  display: flex;
+  justify-content: right;
+  align-items: flex-start;
+  height: 100%;
+`;
+
+const MainContainer = styled.div`
+  top: 0;
+  display: grid;
+  grid-template-columns: 1fr 4.7fr;
+  margin: 0 auto;
+  width: 100%;
+  `;

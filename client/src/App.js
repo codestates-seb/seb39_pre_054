cc6d4c4b4fb0a38dbfbcfe7654719d8dc20b4edf
YeomHyeJi <haji3354@naver.com>
2022-09-05 10:05:26 +0900

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import React ,{ useEffect }from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { loginSuccess, logout } from "./actions";
+import { loginSuccess } from "./actions";
 
 import Main from "./pages/Main";
 import Ask from "./pages/Ask";
@@ -12,14 +12,15 @@ import Login from "./pages/Login.js";
 import Signup from "./pages/Signup.js";
 import styled from "styled-components";
 import QuestionView from "./pages/QuestionView";
-import Posts from "./pages/Posts";
+import QuestionEdit from "./pages/QuestionEdit";
 import FooterLayout from "./components/FooterLayout";
+import AnswerEdit from "./pages/AnswerEdit";
 import MyPageEdit from "./pages/MyPageEdit";
 
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    if(localStorage.getItem("authorization") !== null){
+    if(localStorage.getItem("authorization") !== undefined){
       dispatch(loginSuccess());
     }
   }, [])
@@ -42,8 +43,9 @@ const App = () => {
               ></Route>
               <Route path="/questions/ask" element={<Ask></Ask>}></Route>
               <Route path="/users/:id" element={<MyPage></MyPage>}></Route>
+              <Route path="/questionedit/:id" element={<QuestionEdit></QuestionEdit>}></Route>
+              <Route path="/answeredit/:id" element={<AnswerEdit></AnswerEdit>}></Route>
               <Route path="/users/edit/:id" element = {<MyPageEdit></MyPageEdit>}></Route>
-              <Route path="/posts/:id" element={<Posts></Posts>}></Route>
             </Route>
             <Route path="/users/login" element={<Login></Login>}></Route>
             <Route path="/users/signup" element={<Signup></Signup>}></Route>
